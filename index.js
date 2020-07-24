5f0c80b2da94570ad984f5e6b39c293ad932f192
Hector Hernandez <hectorohernz@gmail.com>
2020-07-24 08:35:06 -0400

    script

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql2');
+
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
@@ -6,6 +8,23 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Ilovetoby',
+    database: 'todo' //dont have this yet
+});
+
+
+try {
+    connection.connect();
+    console.log("Sql has connected");
+
+   } catch (e) {
+    console.log('Oops. Connection to MySQL failed.');
+    console.log(e);
+}
+
 app.post('/add', (req, res) => {
     console.log(req.body);
     res.status(200).send("Post request received")
@@ -13,7 +32,7 @@ app.post('/add', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, (req,res) => {
+app.listen(PORT, (req, res) => {
     console.log(`The applcation is active on port ${PORT}`);
 });
 
